14195283f4d8fb576d1c93cf5672786c754ae0d3
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2026-03-09 10:39:24 -0300

    Project creation

---
+++ src/test/java/com/dev/sb_mongodb/SbMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.sb_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
