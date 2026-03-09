f4c9f66dfa9bbd8d4ef033030ec9f1d398087777
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2026-03-09 15:58:12 -0300

    User findById implemented

--- src/main/java/com/dev/sb_mongodb/services/UserService.java
+++ src/main/java/com/dev/sb_mongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.dev.sb_mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dev.sb_mongodb.domain.User;
 import com.dev.sb_mongodb.repository.UserRepository;
+import com.dev.sb_mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return uRepo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = uRepo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object Not Found!"));
 	}
 }
