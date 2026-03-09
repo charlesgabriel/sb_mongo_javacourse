cef8875e33e4406be8a3b28ef9faabf39f1d7616
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2026-03-09 16:29:41 -0300

    User insert implemented

--- src/main/java/com/dev/sb_mongodb/services/UserService.java
+++ src/main/java/com/dev/sb_mongodb/services/UserService.java
@@ -7,20 +7,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dev.sb_mongodb.domain.User;
+import com.dev.sb_mongodb.dto.UserDTO;
 import com.dev.sb_mongodb.repository.UserRepository;
 import com.dev.sb_mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	@Autowired
-	private UserRepository uRepo;
+	private UserRepository repository;
 
 	public List<User> findAll() {
-		return uRepo.findAll();
+		return repository.findAll();
 	}
 
 	public User findById(String id) {
-		Optional<User> obj = uRepo.findById(id);
+		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object Not Found!"));
+	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
