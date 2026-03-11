295cd82287d1988bb8dcbb6ccb6650e7f30364a3
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2026-03-11 12:20:10 -0300

    Post entity and repository creation, post with nested user added

--- src/main/java/com/dev/sb_mongodb/config/Instantiation.java
+++ src/main/java/com/dev/sb_mongodb/config/Instantiation.java
@@ -1,27 +1,47 @@
 package com.dev.sb_mongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.dev.sb_mongodb.domain.Post;
 import com.dev.sb_mongodb.domain.User;
+import com.dev.sb_mongodb.repository.PostRepository;
 import com.dev.sb_mongodb.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+		Post post1 = new Post(null, 
+				sdf.parse("21/03/2018"), 
+				"Partiu viagem", 
+				"Vou viajar para São Paulo. Abraços!", 
+				maria);
+		Post post2 = new Post(null, 
+				sdf.parse("21/03/2018"), 
+				"Bom dia", 
+				"Acordei feliz hoje!", 
+				maria);
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
