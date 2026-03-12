e0b3a0ecd66e0e9bdf101dbe9e2126099524ac1e
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2026-03-12 13:29:55 -0300

    Post findById implemented

---
+++ src/main/java/com/dev/sb_mongodb/services/PostService.java
@@ -0,0 +1,21 @@
+package com.dev.sb_mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dev.sb_mongodb.domain.Post;
+import com.dev.sb_mongodb.repository.PostRepository;
+import com.dev.sb_mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object Not Found!"));
+	}
+}
