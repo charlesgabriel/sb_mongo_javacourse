2a02b6ca86bb81277499a25f4a57821e071d471e
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2026-03-13 16:35:17 -0300

    Query with multiple criteria

--- src/main/java/com/dev/sb_mongodb/services/PostService.java
+++ src/main/java/com/dev/sb_mongodb/services/PostService.java
@@ -1,5 +1,7 @@
 package com.dev.sb_mongodb.services;
 
+import java.time.Duration;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +25,12 @@ public class PostService {
 	public List<Post> findByTitle(String text) {
 		// return repository.findByTitleContainingIgnoreCase(text);
 		return repository.searchTitle(text);
+	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		// maxDate = new Date(maxDate.getTime() + 23 * 59 * 59 * 999);
+		Duration duration = Duration.ofHours(23);
+		maxDate = new Date(maxDate.getTime() + duration.toMillis());
+		return repository.fullSearch(text, minDate, maxDate);
 	}
 }
